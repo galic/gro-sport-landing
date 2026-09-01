052f70d01e8e9bf4f7c4f01bc7cf6bee8f637d30
lex <agroshef@gmail.com>
2026-09-01 15:21:32 +0500

    fix

--- deploy.js
+++ deploy.js
@@ -5,13 +5,9 @@ var remote_server = {
     port: process.env.SCP_PORT,
     username: process.env.SCP_USER,
     password: process.env.SCP_PASSWORD,
-    // forceIPv4: boolean,  //Connection allow only via resolved IPv4 address (true/false)
-    // forceIPv6: boolean,  //Connection allow only via resolved IPv6 address (true/false)
-    // privateKey: fs.readFileSync('./key.pem'),
-    // passphrase: 'your key passphrase', 
 }
 
-console.log(remote_server)
+//console.log(remote_server)
 
 const local_folder_path = './www';
 const detination_folder_path = '/home/c75424/grosport.ru/www';
@@ -21,23 +17,15 @@ send_folder_using_async_await(local_folder_path, detination_folder_path);
 async function send_folder_using_async_await(folder_path, destination_path) {
     try {
         const client = await Client(remote_server)
-        // client.addListener("end", (props) => {
-        //     console.log('props', props)
-        // })
-
-        // if (await client.exists(detination_folder_path)) {
-        //     console.log('Remove folder ' + detination_folder_path)
-        //     await client.rmdir(detination_folder_path)
-        // }
         //очистка папки
         await client.emptyDir(detination_folder_path)
-        
+
         console.log('Deploy folder from ' + local_folder_path)
 
         await client.uploadDir(folder_path, destination_path)
         client.close()
-        console.log('Done')
+        console.log('Завершено!')
     } catch (e) {
-        console.log(e)
+        console.log('Ошибка', e)
     }
 }
